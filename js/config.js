6e96c269653346cff4dd38838a1e84bb3816ce31
juggernaut09 <venkatteja.ravi@gmail.com>
2024-05-23 22:31:00 -0400

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_M2sAYgzKy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5d78b8k80opt2dnhuh4umqruka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://6m3jri1cj3.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
